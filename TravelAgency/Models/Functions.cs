71dbbb4954efcf41cdcea37e1d0ef4c1336187aa
Herbert Li <2729415988@qq.com>
2023-12-18 17:26:50 +0800

    登陆页面完成

---
+++ TravelAgency/Models/Functions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EHotal.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private string Constr;
+        private SqlDataAdapter sda;
+
+        public int setData(string Query)
+        {
+            int Cnt;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            Cnt = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return Cnt;
+        }
+
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, Constr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public Functions()
+        {
+            Constr = @"";
+            Con = new SqlConnection(Constr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+    }
+
+
+}
